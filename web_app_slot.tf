c8a65ae35e39ec165415c7727d73d2330c0287f6
Agbera <126923646+Agbera@users.noreply.github.com>
2023-10-02 19:12:42 -0400

    Update web_app_slot.tf

--- web_app_slot.tf
+++ web_app_slot.tf
@@ -1,28 +1,28 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "lagos" {
   name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example-plan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                = "mcit-plan"
+  resource_group_name = azurerm_resource_group.mcit.name
+  location            = azurerm_resource_group.mcit.location
   os_type             = "Windows"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_windows_web_app" "example" {
-  name                = "example-windows-web-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+resource "azurerm_windows_web_app" "mcit2" {
+  name                = "mcit-windows-web-app"
+  resource_group_name = azurerm_resource_group.mcit.name
+  location            = azurerm_service_plan.mcit.location
+  service_plan_id     = azurerm_service_plan.mcit.id
 
   site_config {}
 }
 
-resource "azurerm_windows_web_app_slot" "example" {
-  name           = "example-slot"
-  app_service_id = azurerm_windows_web_app.example.id
+resource "azurerm_windows_web_app_slot" "mcit3" {
+  name           = "mcit-slot"
+  app_service_id = azurerm_windows_web_app.mcit.id
 
   site_config {}
 }

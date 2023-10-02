d926d21730f4ccfc731622c8841582506f009f25
Agbera <126923646+Agbera@users.noreply.github.com>
2023-10-02 19:11:15 -0400

    Update web_app.tf

--- web_app.tf
+++ web_app.tf
@@ -1,21 +1,21 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "montreal" {
+  name     = "mcit-resources"
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_service_plan" "mcit1" {
+  name                = "mcit"
+  resource_group_name = azurerm_resource_group.mcit.name
+  location            = azurerm_resource_group.mcit.location
   sku_name            = "P1v2"
   os_type             = "Windows"
 }
 
-resource "azurerm_windows_web_app" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+resource "azurerm_windows_web_app" "mcit" {
+  name                = "mcit"
+  resource_group_name = azurerm_resource_group.mcit.name
+  location            = azurerm_service_plan.mcit.location
+  service_plan_id     = azurerm_service_plan.mcit.id
 
   site_config {}
 }

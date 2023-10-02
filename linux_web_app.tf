e8c15c46e1cdb64118d3d5de69f3cd65d66ff603
Agbera <126923646+Agbera@users.noreply.github.com>
2023-10-02 19:07:40 -0400

    Update linux_web_app.tf

--- linux_web_app.tf
+++ linux_web_app.tf
@@ -1,21 +1,21 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "linuxwebapp" {
   name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                = "mcit"
+  resource_group_name = azurerm_resource_group.mcit.name
+  location            = azurerm_resource_group.mcit.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+resource "azurerm_linux_web_app" "mcit" {
+  name                = "mcit"
+  resource_group_name = azurerm_resource_group.mcit.name
+  location            = azurerm_service_plan.mcit.location
+  service_plan_id     = azurerm_service_plan.mcit.id
 
   site_config {}
 }
